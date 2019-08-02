f405e200a0e2488618b18b45b048fa1b9b32f2e4
mizhang <mizhang@localhost.localdomain>
2019-08-02 14:44:50 +0800

    1

---
+++ fio.py
@@ -0,0 +1,30 @@
+import os
+
+def mydir(name):
+    path="/home/fioresult/"+name
+    try:
+        if(not os.path.exists(path)):
+            os.makedirs(path)
+        return path
+    except:
+        print("mkdir error")
+        return None
+
+if __name__ == "__main__":
+    rw_list=['read']
+    bs_list=['4k']
+    iodepth_list=['1']
+    for rw in rw_list:
+        for bs in bs_list:
+            for iod in iodepth_list:
+                arg=str(rw)+"_"+str(bs)+"_"+str(iod)
+                dir=mydir(arg)
+                if dir:
+                    cmdbase='fio --rw=%s --bs=%s --iodepth=%s --runtime=1m --direct=1 --filename=/mnt/read_64k_8 --name=job1 --ioengine=libaio --thread --group_reporting --numjobs=16 --size=512MB --time_based --output=%s &> /dev/null'
+                    cmd=cmdbase%(rw,bs,iod,dir+"/result")
+                    try:
+                        print("now: "+arg)
+                        os.system(cmd)
+                    except:
+                        print(arg)
+                    
